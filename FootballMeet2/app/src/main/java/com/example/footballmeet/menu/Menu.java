d61b5909e3dd9cd11a74670df66a14144de2b53f
braisd11 <braisdiaz97@gmail.com>
2024-05-15 15:19:57 +0200

    interfaz menu acabada

--- FootballMeet2/app/src/main/java/com/example/footballmeet/menu/Menu.java
+++ FootballMeet2/app/src/main/java/com/example/footballmeet/menu/Menu.java
@@ -2,7 +2,9 @@ package com.example.footballmeet.menu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.footballmeet.R;
 
@@ -12,5 +14,43 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void onClickBtnMenu(View view) {
+
+        Intent intent= null;
+
+        switch (view.getId()){
+            case R.id.btn_partidosMenu:
+
+                finish();
+
+                break;
+
+            case R.id.btn_perfilMenu:
+
+                finish();
+
+                break;
+
+            case R.id.btn_equiposMenu:
+
+                finish();
+
+                break;
+
+            case R.id.btn_torneosMenu:
+
+                finish();
+
+                break;
+        }
+
+        if (intent!=null){
+
+            startActivity(intent);
+
+        }
+
     }
 }
